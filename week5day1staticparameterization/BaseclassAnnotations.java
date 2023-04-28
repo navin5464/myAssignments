de496c58501d418dd7aed73abcc81c384ddac708
navin5464 <72960574+navin5464@users.noreply.github.com>
2023-04-28 14:20:37 +0530

    Add files via upload

---
+++ week5day1staticparameterization/BaseclassAnnotations.java
@@ -0,0 +1,52 @@
+package week5day1staticparameterization;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+
+import week5day2.Readexcelintegration;
+
+public class BaseclassAnnotations {
+	public String filename;
+	public RemoteWebDriver driver;
+	
+	@Parameters({"username","password","url","browser"})
+	
+	  @BeforeMethod
+      public void Preconditions(String username,String password,String url,String browser) {
+		if (browser.equalsIgnoreCase("chrome")) {
+			 driver  = new ChromeDriver();
+		}else if (browser.equalsIgnoreCase("edge")) {
+			driver  = new EdgeDriver();
+		}
+		 
+  		driver.manage().window().maximize();
+  		driver.get(url);
+  		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+  		driver.findElement(By.id("username")).sendKeys(username);
+  		driver.findElement(By.id("password")).sendKeys(password);
+  		driver.findElement(By.className("decorativeSubmit")).click();
+  		driver.findElement(By.linkText("CRM/SFA")).click();
+	}
+      @AfterMethod
+      public void Postconditions() {
+    	  driver.close();
+      }
+      
+      @DataProvider(name="fetchdata")
+  	public String[][] senddata() throws IOException {
+  		String [][] readData= Readexcelintegration.readData(filename);
+  		
+  		return readData;
+}
+      }
+
+
---
+++ week5day1staticparameterization/CreateLead.java
@@ -0,0 +1,41 @@
+package week5day1staticparameterization;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import week5day2.Readexcelintegration;
+
+public class CreateLead extends BaseclassAnnotations {
+	@BeforeTest
+	public void setData() {
+		filename="CreateLead";
+	}
+	
+		 @Test (dataProvider="fetchdata")
+			public  void runCreateLead(String cname,String fname,String lname,String ph) {
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys(cname);
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys(fname);
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys(lname);
+		driver.findElement(By.id("createLeadForm_primaryPhoneNumber")).sendKeys(ph);
+		driver.findElement(By.name("submitButton")).click();
+		String text = driver.findElement(By.id("viewLead_companyName_sp")).getText();
+		if (text.contains("Testleaf")) {
+			System.out.println("Lead created successfully");
+		}
+		else {
+			System.out.println("Lead is not created");
+		}
+		
+
+	}
+	}
+
+
